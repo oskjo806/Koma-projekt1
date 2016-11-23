2857b07335d04dd77103e854245fe062f5c93bc2
samrisxxl <samri821@student.liu.se>
2016-11-23 15:57:50 +0100

    Add files via upload

--- Demo/Script/app.js
+++ Demo/Script/app.js
@@ -1,16 +1,51 @@
-function saveData(String v, String s){
+String dataVarName = "MissionData";
+String expVarName = "ExpValue";
+
+function saveData(String varName, String value){
 	if (typeof(Storage) !== "undefined") {
-    	localStorage.setItem(v, s);
+    	localStorage.setItem(varName, value);
     	return true;
 	} else {
     	return false;
 	}
 }
 
-function loadData(){
+function getData(varName){
+	var data = "";
 	if (typeof(Storage) !== "undefined") {
-    	// Code for localStorage/sessionStorage.
+    	data = localStorage.getItem(varName);
+    	if(data === null){
+    		return "";
+    	}
 	} else {
     	return false;
 	}
+	return data;
+}
+
+function createMission(mission){
+	//to-do
+}
+
+function getAllMissionData(){
+	var data = JSON.parse(getData(dataVarName));
+	Mission missions = new Mission[data.length];
+
+	for(int i = 0; i < data.length; ++i){
+		missions[i] = createMission(data.missions[i]);
+	}
+	return missions;
+}
+
+function getExp(){
+	String experience = getData(expVarName);
+	if(experience == ""){
+		saveData(expVarName, 0);
+		return 0;
+	}
+	return parseInt(experience);
+}
+
+function setExperience(int exp){
+	saveData(expVarName, exp);
 }
